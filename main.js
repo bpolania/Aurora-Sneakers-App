bc67be09e77a50da3d656fcc2245af29aea9e9db
Boris Polania <boris.polania@gmail.com>
2023-03-15 16:29:45 -0700

    Final

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
 require('dotenv').config();
-const fs = require('fs');
-const ethers = require('ethers');
+const Web3 = require('web3');
+const { abi } = require('./SneakerFactory.json');
 
-const privateKey = process.env.PRIVATE_KEY;
-const provider = new ethers.providers.InfuraProvider('mainnet', process.env.INFURA_PROJECT_ID);
 
-const wallet = new ethers.Wallet(privateKey, provider);
+const providerUrl = 'https://mainnet.aurora.dev';
+const web3 = new Web3(providerUrl);
 
-const sneakerFactoryAddress = '<YOUR_SNEAKER_FACTORY_CONTRACT_ADDRESS>';
-const sneakerFactoryABI = JSON.parse(fs.readFileSync('sneakerFactoryABI.json', 'utf8'));
+const contractABI = [{"inputs":[{"internalType":"address","name":"admin","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"previousAdminRole","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"newAdminRole","type":"bytes32"}],"name":"RoleAdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":true,"internalType":"address","name":"sender","type":"address"}],"name":"RoleGranted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":true,"internalType":"address","name":"sender","type":"address"}],"name":"RoleRevoked","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"nftAddress","type":"address"},{"indexed":true,"internalType":"address","name":"creator","type":"address"}],"name":"SneakerNFTCreated","type":"event"},{"inputs":[],"name":"ADMIN_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"DEFAULT_ADMIN_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"createSneakerNFT","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getNFTContracts","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"}],"name":"getRoleAdmin","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"grantRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"hasRole","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"nfts","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"renounceRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"revokeRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}];
 
-const sneakerFactoryContract = new ethers.Contract(sneakerFactoryAddress, sneakerFactoryABI, wallet);
+const contractAddress = "0xe27cE36c548924dB4c7A4C1CE7BbF6494762925F"; // Replace with your deployed contract address
 
-async function createSneakerNFT() {
-  const tx = await sneakerFactoryContract.createSneakerNFT();
-  const receipt = await tx.wait();
-  console.log('Sneaker NFT Created:', receipt.events[0].args.nftAddress);
-}
+const main = async () => {
+  try {
+    const accounts = await web3.eth.getAccounts();
+    const sneakerFactory = new web3.eth.Contract(contractABI, contractAddress);
 
-async function getNFTContracts() {
-  const nfts = await sneakerFactoryContract.getNFTContracts();
-  console.log('NFT Contracts:', nfts);
-}
+    // Call the getNFTContracts function to get the list of deployed SneakerNFT contracts
+    const nftContracts = await sneakerFactory.methods.getNFTContracts().call();
+    console.log("Deployed SneakerNFT contracts:", nftContracts);
 
-// Run the desired function
-createSneakerNFT();
-// getNFTContracts();
+    // Listen for the SneakerNFTCreated event
+    sneakerFactory.events.SneakerNFTCreated((error, event) => {
+      if (error) {
+        console.error("Event error:", error);
+      } else {
+        console.log(`New SneakerNFT created by ${event.returnValues.creator}: ${event.returnValues.nftAddress}`);
+      }
+    });
+
+    // Call the createSneakerNFT function to create a new SneakerNFT contract
+    const createSneakerNFTTx = await sneakerFactory.methods.createSneakerNFT().send({ from: accounts[0] });
+    console.log("SneakerNFT created!");
+
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+main();
+
+
+
